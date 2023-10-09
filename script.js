e8b92eb5860a40b2d630862e5775b8718bb7dc15
SnakeRuse <snakeice2000@mail.ru>
2023-10-09 03:49:14 +0300

    decomposition code to files

--- script.js
+++ script.js
@@ -1,70 +1,81 @@
 class AppCommon extends HTMLElement {
   constructor() {
-    super();
-  };
+    super()
+  }
 
   async _setContent(pathToContent) {
-    const contentStr = await fetch(pathToContent).then(res => res.text());
-    const htmlView = new DOMParser().parseFromString(contentStr, 'text/html');
-    const content = {
-      style: htmlView.head.querySelector('style'),
-      html: htmlView.head.querySelector('template').content.cloneNode(true),
+    const getFileText = async (type) => {
+      const types = {
+        css: 'style.css',
+        html: 'index.html',
+        js: 'script.js',
+      }
+      return await fetch(`${pathToContent}/${types[type]}`)
+        .then(res => res.text())
     }
-    const script = document.createElement('script');
-    script.innerHTML = htmlView.head.querySelector('script').innerHTML;
-    const docFragment = document.createDocumentFragment();
-    docFragment.appendChild(content.style);
-    docFragment.appendChild(content.html);
-    docFragment.appendChild(script);
-    this.replaceWith(docFragment);
-  };
+
+    const styletStr = await getFileText('css')
+    const style = document.createElement('style')
+    style.innerHTML = styletStr
+
+    const contentStr = await getFileText('html')
+    const htmlView = new DOMParser().parseFromString(contentStr, 'text/html')
+    const content = htmlView.head.querySelector('template').content.cloneNode(true)
+    
+    const scriptStr = await getFileText('js')
+    const script = document.createElement('script')
+    script.innerHTML = scriptStr
+
+    const docFragment = document.createDocumentFragment()
+    docFragment.appendChild(style)
+    docFragment.appendChild(content)
+    docFragment.appendChild(script)
+    this.replaceWith(docFragment)
+  }
 }
 
 class AppSample extends AppCommon {
   constructor() {
-    super();
-    this.attachShadow({ mode: 'open' });
-  };
+    super()
+    this.attachShadow({ mode: 'open' })
+  }
 
   async #routing() {
-    const { hash } = window.location;
-    const view = hash ? hash.substring(1) : '/home';
-    const pathToView = `/src/views${view}/index.html`;
-    this._setContent(pathToView);
+    const { hash } = window.location
+    this._setContent(`/src/views${hash ? hash.substring(1) : '/home'}`)
   }
 
   connectedCallback() {
-    this.#routing(); 
-    window.addEventListener('hashchange', () => this.#routing());
-  };
-};
+    this.#routing() 
+    window.addEventListener('hashchange', () => this.#routing())
+  }
+}
 
 class AppComponent extends AppCommon {
   constructor() {
-    super();
+    super()
   }
 
   connectedCallback() {
-    const component = this.getAttribute('html-component');
-    const pathToComponent = `/src${component}/index.html`;
-    this._setContent(pathToComponent);
+    const component = this.getAttribute('html-component')
+    this._setContent( `/src${component}`)
   }
-};
+}
 
 class AppLink extends HTMLElement {
   constructor() {
-    super();
+    super()
   }
 
   connectedCallback() {
     this.addEventListener('click', () => {
-      window.location.hash = this.getAttribute('to');
+      window.location.hash = this.getAttribute('to')
     })
   }
-};
+}
 
 customElements.define('app-sample', AppSample)
 customElements.define('app-component', AppComponent)
 customElements.define('app-link', AppLink)
 
-if (window.location.pathname === '/index.html') window.location.pathname = '';
+if (window.location.pathname === '/index.html') window.location.pathname = ''
